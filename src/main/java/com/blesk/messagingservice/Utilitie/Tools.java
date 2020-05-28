9c0d3f67aed710bc55cfd48d52a3d6a76d159896
Bence <m.bence05@gmail.com>
2020-05-28 20:56:56 +0200

    odstranenie suboru Tools

--- src/main/java/com/blesk/messagingservice/Utilitie/Tools.java
+++
@@ -1,8 +0,0 @@
-package com.blesk.messagingservice.Utilitie;
-
-public class Tools {
-
-    public static <T> T getNotNull(T a, T b) {
-        return b != null && a != null && !a.equals(b) ? a : b;
-    }
-}
