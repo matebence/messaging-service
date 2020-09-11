14f27f27c3e2484046781a991f4d867ac4c8c9ea
Bence <m.bence05@gmail.com>
2020-09-11 16:30:20 +0200

    zmena executeInfoRequest pre 403 Invalid cors request

--- src/main/java/com/blesk/messagingservice/Config/WebSocketConfig.java
+++ src/main/java/com/blesk/messagingservice/Config/WebSocketConfig.java
@@ -1,13 +1,24 @@
 package com.blesk.messagingservice.Config;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.sockjs.client.InfoReceiver;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
 
 @Configuration
-public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer implements WebSocketMessageBrokerConfigurer, InfoReceiver {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
@@ -25,5 +36,20 @@ public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfi
     @Override
     protected boolean sameOriginDisabled() {
         return true;
+    }
+
+    @Override
+    public String executeInfoRequest(URI infoUrl, HttpHeaders headers) {
+        HttpGet httpGet = new HttpGet(infoUrl);
+        HttpClient httpClient = HttpClients.createDefault();
+
+        try {
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            List<String> output = IOUtils.readLines(httpResponse.getEntity().getContent());
+
+            return output.get(0);
+        } catch (IOException ex) {
+            return null;
+        }
     }
 }
