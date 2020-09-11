d6d9be24cdd68735da53c7f24de9a81130d7d216
Bence <m.bence05@gmail.com>
2020-09-11 21:22:58 +0200

    UrlBasedCorsConfigurationSource

--- src/main/java/com/blesk/messagingservice/Config/SpringSecurity.java
+++ src/main/java/com/blesk/messagingservice/Config/SpringSecurity.java
@@ -1,9 +1,9 @@
 package com.blesk.messagingservice.Config;
 
-import com.google.cloud.storage.HttpMethod;
-import com.netflix.ribbon.proxy.annotation.Http;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,8 +14,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 @Configuration
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
@@ -26,15 +24,21 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setMaxAge(3600L);
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200, http://localhost:4300, http://localhost:4400".split(", ")));
+        config.setAllowedHeaders(Arrays.asList("X-Requested-With, Origin, Content-Type, Accept, Authorization, X-Auth-Token".split(", ")));
+        config.setAllowedMethods(Arrays.asList("HEAD, GET, PUT, POST, DELETE, PATCH, OPTIONS".split(", ")));
+        config.setExposedHeaders(Arrays.asList("X-Auth-Token, Authorization".split(", ")));
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return filterRegistrationBean;
     }
 
     @Bean
